b123d3ed9b63f271dd9939815fe4c9c0cf6bc146
Ryohei Sasaki <ryohei.sasaki@akane.waseda.jp>
2019-11-11 16:21:14 +0900

    Update ndt_mapping

--- include/ndt_mapping.h
+++ include/ndt_mapping.h
@@ -36,58 +36,29 @@ private:
   ros::Subscriber points_sub;
 
   struct pose{double x,y,z;double roll,pitch,yaw;};
-  struct pose previous_pose; 
-  struct pose guess_pose;
   struct pose current_pose;
-  struct pose ndt_pose;
-  struct pose added_pose;
-  struct pose localizer_pose;
-
-  ros::Time current_scan_time,previous_scan_time;
-  ros::Duration scan_duration;
-
-  double diff , diff_x, diff_y , diff_z , diff_yaw;  // current_pose - previous_pose
-
-  double current_velocity_x , current_velocity_y , current_velocity_z ;
-  
+  struct pose previous_pose;
 
   pcl::PointCloud<pcl::PointXYZI> map;
-
+  pcl::VoxelGrid<pcl::PointXYZI> voxel_grid_filter;
   pcl::NormalDistributionsTransform<pcl::PointXYZI, pcl::PointXYZI> ndt;
 
   // Default values
   int max_iter ;        // Maximum iterations
-  //static float ndt_res = 1.0;      // Resolution
   float ndt_res ;      // Resolution
   double step_size ;   // Step size
   double trans_eps ;  // Transformation epsilon
 
-  // Leaf size of VoxelGrid filter.
-  double voxel_leaf_size;
+  double voxel_leaf_size;// Leaf size of VoxelGrid filter.
 
-  ros::Time callback_start, callback_end, t1_start, t1_end, t2_start, t2_end, t3_start, t3_end, t4_start, t4_end,
-    t5_start, t5_end;
-  ros::Duration d_callback, d1, d2, d3, d4, d5;
-
-  ros::Publisher ndt_map_pub, current_pose_pub,guess_pose_linaer_pub;
-  geometry_msgs::PoseStamped current_pose_msg, guess_pose_msg;
-
-  ros::Publisher ndt_stat_pub;
-  std_msgs::Bool ndt_stat_msg;
+  ros::Publisher ndt_map_pub, current_pose_pub;
+  geometry_msgs::PoseStamped current_pose_msg;
 
   int initial_scan_loaded;
-
-  Eigen::Matrix4f gnss_transform = Eigen::Matrix4f::Identity();
-
-  double min_scan_range;
-  //static double min_scan_range = 1.0;
-  double max_scan_range;
   double min_add_scan_shift;
-  //static double min_add_scan_shift = 0.5;
 
   double _tf_x, _tf_y, _tf_z, _tf_roll, _tf_pitch, _tf_yaw;
   Eigen::Matrix4f tf_btol, tf_ltob;//base_link2localizer等の略?
-
 
   bool _incremental_voxel_update;
 
@@ -95,17 +66,11 @@ private:
   bool has_converged;
   int final_num_iteration;
   double transformation_probability;
-
+  
+  bool is_first_map;
 
   std::ofstream ofs;
   std::string filename;
 
-  void timerCallback(const ros::TimerEvent &e);
-  
-  double calcDiffForRadian(const double lhs_rad, const double rhs_rad);
-
-
   void points_callback(const sensor_msgs::PointCloud2::ConstPtr& input);
-
-  void publishMapAndPose();
 };
--- src/ndt_mapping.cpp
+++ src/ndt_mapping.cpp
@@ -7,38 +7,22 @@ ndt_mapping::ndt_mapping()
   ndt_map_pub = nh.advertise<sensor_msgs::PointCloud2>("/ndt_map", 1000);
   current_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/current_pose", 1000);
 
-  diff = 0.0, diff_x = 0.0, diff_y = 0.0, diff_z = 0.0, diff_yaw= 0.0;  // current_pose - previous_pose
-
-  current_velocity_x = 0.0, current_velocity_y = 0.0, current_velocity_z = 0.0;
-
-
   // Default values
   max_iter = 30;        // Maximum iterations
-  //static float ndt_res = 1.0;      // Resolution
   ndt_res = 5.0;      // Resolution
   step_size = 0.1;   // Step size
   trans_eps = 0.01;  // Transformation epsilon
 
-  // Leaf size of VoxelGrid filter.
-  voxel_leaf_size = 2.0;
+  voxel_leaf_size = 2.0;// Leaf size of VoxelGrid filter.
 
   initial_scan_loaded = 0;
-
- 
-  min_scan_range = 5.0;
-  //static double min_scan_range = 1.0;
-  max_scan_range = 200.0;
   min_add_scan_shift = 1.0;
-  //static double min_add_scan_shift = 0.5;
 
   _tf_x=0.0, _tf_y=0.0, _tf_z=0.0, _tf_roll=0.0, _tf_pitch=0.0, _tf_yaw=0.0;
 
   std::cout << "incremental_voxel_update: " << _incremental_voxel_update << std::endl;
-
-
   std::cout << "(tf_x,tf_y,tf_z,tf_roll,tf_pitch,tf_yaw): (" << _tf_x << ", " << _tf_y << ", " << _tf_z << ", "
             << _tf_roll << ", " << _tf_pitch << ", " << _tf_yaw << ")" << std::endl;
-
 
   Eigen::Translation3f tl_btol(_tf_x, _tf_y, _tf_z);                 // tl: translation
   Eigen::AngleAxisf rot_x_btol(_tf_roll, Eigen::Vector3f::UnitX());  // rot: rotation
@@ -49,32 +33,25 @@ ndt_mapping::ndt_mapping()
 
   map.header.frame_id = "map";
 
-  previous_pose.x = previous_pose.y = previous_pose.z = 0.0;previous_pose.roll = previous_pose.pitch = previous_pose.yaw = 0.0;
-  ndt_pose.x = ndt_pose.y = ndt_pose.z = 0.0;ndt_pose.roll = ndt_pose.pitch = ndt_pose.yaw = 0.0;
   current_pose.x = current_pose.y = current_pose.z = 0.0;current_pose.roll = current_pose.pitch = current_pose.yaw = 0.0;
-  guess_pose.x = guess_pose.y = guess_pose.z = 0.0;guess_pose.roll = guess_pose.pitch = guess_pose.yaw = 0.0;
-  added_pose.x = added_pose.y = added_pose.z = 0.0;added_pose.roll = added_pose.pitch = added_pose.yaw = 0.0;
+  previous_pose.x = previous_pose.y = previous_pose.z = 0.0;previous_pose.roll = previous_pose.pitch = previous_pose.yaw = 0.0;
 
-  diff_x = 0.0;diff_y = 0.0;diff_z = 0.0;diff_yaw = 0.0;
+  voxel_grid_filter.setLeafSize(voxel_leaf_size, voxel_leaf_size, voxel_leaf_size);  
+
+  ndt.setTransformationEpsilon(trans_eps);
+  ndt.setStepSize(step_size);
+  ndt.setResolution(ndt_res);
+  ndt.setMaximumIterations(max_iter);
+
+  is_first_map = true;
 
 }; 
 
 ndt_mapping::~ndt_mapping(){}; 
 
-
-double ndt_mapping::calcDiffForRadian(const double lhs_rad, const double rhs_rad)
-{
-  double diff_rad = lhs_rad - rhs_rad;
-  if      (diff_rad >= M_PI) diff_rad = diff_rad - 2 * M_PI;
-  else if (diff_rad < -M_PI) diff_rad = diff_rad + 2 * M_PI;
-  return diff_rad;
-}
-
 void ndt_mapping::points_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
 {
-  double r;
-  pcl::PointXYZI p;
-  pcl::PointCloud<pcl::PointXYZI> tmp, scan;
+  pcl::PointCloud<pcl::PointXYZI> scan;
   pcl::PointCloud<pcl::PointXYZI>::Ptr filtered_scan_ptr(new pcl::PointCloud<pcl::PointXYZI>());
   pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_scan_ptr(new pcl::PointCloud<pcl::PointXYZI>());
   tf::Quaternion q;
@@ -84,19 +61,7 @@ void ndt_mapping::points_callback(const sensor_msgs::PointCloud2::ConstPtr& inpu
   static tf::TransformBroadcaster br;
   tf::Transform transform;
 
-  current_scan_time = input->header.stamp;
-
-  pcl::fromROSMsg(*input, tmp);
-
-  for (pcl::PointCloud<pcl::PointXYZI>::const_iterator item = tmp.begin(); item != tmp.end(); item++)
-  {
-    p.x = (double)item->x; p.y = (double)item->y; p.z = (double)item->z;
-    p.intensity = (double)item->intensity;
-
-    r = sqrt(pow(p.x, 2.0) + pow(p.y, 2.0));
-    if (min_scan_range < r && r < max_scan_range) scan.push_back(p);
-  }
-
+  pcl::fromROSMsg(*input, scan);
   pcl::PointCloud<pcl::PointXYZI>::Ptr scan_ptr(new pcl::PointCloud<pcl::PointXYZI>(scan));
 
   // Add initial point cloud to velodyne_map
@@ -107,44 +72,25 @@ void ndt_mapping::points_callback(const sensor_msgs::PointCloud2::ConstPtr& inpu
     initial_scan_loaded = 1;
   }
 
-  // Apply voxelgrid filter
-  pcl::VoxelGrid<pcl::PointXYZI> voxel_grid_filter;
-  voxel_grid_filter.setLeafSize(voxel_leaf_size, voxel_leaf_size, voxel_leaf_size);
   voxel_grid_filter.setInputCloud(scan_ptr);
   voxel_grid_filter.filter(*filtered_scan_ptr);
-
-  pcl::PointCloud<pcl::PointXYZI>::Ptr map_ptr(new pcl::PointCloud<pcl::PointXYZI>(map));
-
-  ndt.setTransformationEpsilon(trans_eps);
-  ndt.setStepSize(step_size);
-  ndt.setResolution(ndt_res);
-  ndt.setMaximumIterations(max_iter);
   ndt.setInputSource(filtered_scan_ptr);
 
-  static bool is_first_map = true;
+  pcl::PointCloud<pcl::PointXYZI>::Ptr map_ptr(new pcl::PointCloud<pcl::PointXYZI>(map));
   if (is_first_map == true){
     ndt.setInputTarget(map_ptr);
     is_first_map = false;
   }
 
-  guess_pose.x = previous_pose.x + diff_x; guess_pose.y = previous_pose.y + diff_y; guess_pose.z = previous_pose.z + diff_z;
-  guess_pose.roll = previous_pose.roll; guess_pose.pitch = previous_pose.pitch; guess_pose.yaw = previous_pose.yaw + diff_yaw;
-
-  pose guess_pose_for_ndt;
-  guess_pose_for_ndt = guess_pose;
-
-  Eigen::AngleAxisf init_rotation_x(guess_pose_for_ndt.roll, Eigen::Vector3f::UnitX());
-  Eigen::AngleAxisf init_rotation_y(guess_pose_for_ndt.pitch, Eigen::Vector3f::UnitY());
-  Eigen::AngleAxisf init_rotation_z(guess_pose_for_ndt.yaw, Eigen::Vector3f::UnitZ());
-
-  Eigen::Translation3f init_translation(guess_pose_for_ndt.x, guess_pose_for_ndt.y, guess_pose_for_ndt.z);
+  Eigen::Translation3f init_translation(current_pose.x, current_pose.y, current_pose.z);
+  Eigen::AngleAxisf init_rotation_x(current_pose.roll, Eigen::Vector3f::UnitX());
+  Eigen::AngleAxisf init_rotation_y(current_pose.pitch, Eigen::Vector3f::UnitY());
+  Eigen::AngleAxisf init_rotation_z(current_pose.yaw, Eigen::Vector3f::UnitZ());
 
   Eigen::Matrix4f init_guess =
       (init_translation * init_rotation_z * init_rotation_y * init_rotation_x).matrix() * tf_btol;
 
-
   pcl::PointCloud<pcl::PointXYZI>::Ptr output_cloud(new pcl::PointCloud<pcl::PointXYZI>);
-
  
   ndt.align(*output_cloud, init_guess);
   fitness_score = ndt.getFitnessScore();
@@ -157,61 +103,29 @@ void ndt_mapping::points_callback(const sensor_msgs::PointCloud2::ConstPtr& inpu
 
   pcl::transformPointCloud(*scan_ptr, *transformed_scan_ptr, t_localizer);
 
-  tf::Matrix3x3 mat_l, mat_b;
-
-  mat_l.setValue(static_cast<double>(t_localizer(0, 0)), static_cast<double>(t_localizer(0, 1)),
-                 static_cast<double>(t_localizer(0, 2)), static_cast<double>(t_localizer(1, 0)),
-                 static_cast<double>(t_localizer(1, 1)), static_cast<double>(t_localizer(1, 2)),
-                 static_cast<double>(t_localizer(2, 0)), static_cast<double>(t_localizer(2, 1)),
-                 static_cast<double>(t_localizer(2, 2)));
-
+  tf::Matrix3x3 mat_b;
   mat_b.setValue(static_cast<double>(t_base_link(0, 0)), static_cast<double>(t_base_link(0, 1)),
                  static_cast<double>(t_base_link(0, 2)), static_cast<double>(t_base_link(1, 0)),
                  static_cast<double>(t_base_link(1, 1)), static_cast<double>(t_base_link(1, 2)),
                  static_cast<double>(t_base_link(2, 0)), static_cast<double>(t_base_link(2, 1)),
                  static_cast<double>(t_base_link(2, 2)));
 
-  // Update localizer_pose.
-  localizer_pose.x = t_localizer(0, 3);localizer_pose.y = t_localizer(1, 3);localizer_pose.z = t_localizer(2, 3);
-  mat_l.getRPY(localizer_pose.roll, localizer_pose.pitch, localizer_pose.yaw, 1);
-
-  // Update ndt_pose.
-  ndt_pose.x = t_base_link(0, 3);ndt_pose.y = t_base_link(1, 3);ndt_pose.z = t_base_link(2, 3);
-  mat_b.getRPY(ndt_pose.roll, ndt_pose.pitch, ndt_pose.yaw, 1);
-
-  current_pose.x = ndt_pose.x;current_pose.y = ndt_pose.y;current_pose.z = ndt_pose.z;
-  current_pose.roll = ndt_pose.roll;current_pose.pitch = ndt_pose.pitch;current_pose.yaw = ndt_pose.yaw;
+  // Update current_pose.
+  current_pose.x = t_base_link(0, 3);current_pose.y = t_base_link(1, 3);current_pose.z = t_base_link(2, 3);
+  mat_b.getRPY(current_pose.roll, current_pose.pitch, current_pose.yaw, 1);//mat2rpy
 
   transform.setOrigin(tf::Vector3(current_pose.x, current_pose.y, current_pose.z));
-  q.setRPY(current_pose.roll, current_pose.pitch, current_pose.yaw);
-  transform.setRotation(q);
+  q.setRPY(current_pose.roll, current_pose.pitch, current_pose.yaw); //q from rpy
+  transform.setRotation(q);//trans from q
 
-  br.sendTransform(tf::StampedTransform(transform, current_scan_time, "map", "base_link"));
+  br.sendTransform(tf::StampedTransform(transform, input->header.stamp, "map", "base_link"));
 
-  scan_duration = current_scan_time - previous_scan_time;
-  double secs = scan_duration.toSec();
-
-  // Calculate the offset (curren_pos - previous_pos)
-  diff_x = current_pose.x - previous_pose.x;diff_y = current_pose.y - previous_pose.y;diff_z = current_pose.z - previous_pose.z;
-  diff_yaw = calcDiffForRadian(current_pose.yaw, previous_pose.yaw);
-  diff = sqrt(diff_x * diff_x + diff_y * diff_y + diff_z * diff_z);
-
-  current_velocity_x = diff_x / secs; current_velocity_y = diff_y / secs; current_velocity_z = diff_z / secs;
-
-  // Update position and posture. current_pos -> previous_pos
-  previous_pose.x = current_pose.x;previous_pose.y = current_pose.y;previous_pose.z = current_pose.z;
-  previous_pose.roll = current_pose.roll;previous_pose.pitch = current_pose.pitch;previous_pose.yaw = current_pose.yaw;
-
-  previous_scan_time.sec = current_scan_time.sec; previous_scan_time.nsec = current_scan_time.nsec;
-
-  // Calculate the shift between added_pos and current_pos
-  double shift = sqrt(pow(current_pose.x - added_pose.x, 2.0) + pow(current_pose.y - added_pose.y, 2.0));
-  
+  double shift = sqrt(pow(current_pose.x - previous_pose.x, 2.0) + pow(current_pose.y - previous_pose.y, 2.0));
   if (shift >= min_add_scan_shift)
   {
     map += *transformed_scan_ptr;
-    added_pose.x = current_pose.x;added_pose.y = current_pose.y;added_pose.z = current_pose.z;added_pose.roll = current_pose.roll;added_pose.pitch = current_pose.pitch;added_pose.yaw = current_pose.yaw;
-
+    previous_pose.x = current_pose.x;previous_pose.y = current_pose.y;previous_pose.z = current_pose.z;
+    previous_pose.roll = current_pose.roll;previous_pose.pitch = current_pose.pitch;previous_pose.yaw = current_pose.yaw;
     ndt.setInputTarget(map_ptr);
   }
 
@@ -219,9 +133,8 @@ void ndt_mapping::points_callback(const sensor_msgs::PointCloud2::ConstPtr& inpu
   pcl::toROSMsg(*map_ptr, *map_msg_ptr);
   ndt_map_pub.publish(*map_msg_ptr);
 
-  q.setRPY(current_pose.roll, current_pose.pitch, current_pose.yaw);
   current_pose_msg.header.frame_id = "map";
-  current_pose_msg.header.stamp = current_scan_time;
+  current_pose_msg.header.stamp = input->header.stamp;
   current_pose_msg.pose.position.x = current_pose.x;current_pose_msg.pose.position.y = current_pose.y;current_pose_msg.pose.position.z = current_pose.z;
   current_pose_msg.pose.orientation.x = q.x();current_pose_msg.pose.orientation.y = q.y();current_pose_msg.pose.orientation.z = q.z();current_pose_msg.pose.orientation.w = q.w();
 
@@ -243,7 +156,6 @@ void ndt_mapping::points_callback(const sensor_msgs::PointCloud2::ConstPtr& inpu
   std::cout << t_localizer << std::endl;
   std::cout << "shift: " << shift << std::endl;
   std::cout << "-----------------------------------------------------------------" << std::endl;
-
 }
 
 
